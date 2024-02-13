0e74a8058ef10e77592f8cadc6dd9e9a4b622285
H1V35 <higesjorge@gmail.com>
2024-02-13 23:55:36 +0100

    🎨 style: Update styling on ErrorPage

--- technical-tests/03-simple-blog-app/src/pages/Error.tsx
+++ technical-tests/03-simple-blog-app/src/pages/Error.tsx
@@ -2,7 +2,7 @@ import thisIsFine from "../assets/images/this-is-fine-dog.gif";
 
 export default function Page404() {
   return (
-    <section className="px-4 sm:px-8 py-4 flex flex-col items-center gap-8 sm:gap-16">
+    <section className="px-4 sm:px-8 py-4 flex flex-col items-center gap-6 sm:gap-8">
       <h1 className="mt-4 text-4xl sm:text-5xl">Something has gone wrong...</h1>
 
       <img
