2020107fe23e43e48ddc6a9747bcff7df5e2912a
H1V35 <higesjorge@gmail.com>
2024-02-19 14:16:28 +0100

    ✨ feat: Enhance user experience fluidity by adding state as placeholder

--- technical-tests/03-simple-blog-app/src/hooks/useGetPost.ts
+++ technical-tests/03-simple-blog-app/src/hooks/useGetPost.ts
@@ -1,7 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchPost } from "../services";
+import { useLocation } from "react-router-dom";
 
 export function useGetPost(postId: string) {
+  const { state } = useLocation();
   const {
     data: post,
     isLoading,
@@ -10,6 +12,8 @@ export function useGetPost(postId: string) {
   } = useQuery({
     queryKey: ["post", postId],
     queryFn: () => fetchPost(postId),
+    placeholderData: state,
+    enabled: !state,
   });
 
   return { post, isLoading, isError, error };
