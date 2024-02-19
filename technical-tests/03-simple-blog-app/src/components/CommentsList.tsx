1224eb9b5cf1db9604be754073eda914f0d6d54f
H1V35 <higesjorge@gmail.com>
2024-02-19 14:46:41 +0100

    ✨ feat: Create skeleton loader for comments list

--- technical-tests/03-simple-blog-app/src/components/CommentsList.tsx
+++ technical-tests/03-simple-blog-app/src/components/CommentsList.tsx
@@ -7,7 +7,7 @@ export function CommentsList() {
   const { postId } = useParams();
   const { comments, isLoading, isError, error } = useGetComments(postId!);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <CommentsListSkeleton />;
 
   if (isError) return <div>Error: {error?.message}</div>;
 
@@ -23,6 +23,23 @@ export function CommentsList() {
         })}
 
         <CommentForm />
+      </div>
+    </section>
+  );
+}
+
+function CommentsListSkeleton() {
+  return (
+    <section className="w-full gap-4 sm:gap-8 animate-pulse">
+      <h2 className="ml-2 text-2xl sm:text-3xl">Comments</h2>
+      <div className="p-2 sm:px-4 rounded-3xl border border-black flex flex-col gap-2">
+        <div className="h-20 bg-gray-200 rounded-xl"></div>
+        <div className="h-20 bg-gray-200 rounded-xl"></div>
+        <div className="h-20 bg-gray-200 rounded-xl"></div>
+        <div className="w-full h-8 px-1 flex justify-center gap-1">
+          <div className="w-full px-3 py-1 bg-gray-200 rounded-lg"></div>
+          <div className="pl-3 pr-2 w-12 bg-gray-200 rounded-lg"></div>
+        </div>
       </div>
     </section>
   );
